66b5615f1471f837a8adf85b54787acf25aeafdf
MahiAwasthi <mahi2057.be23@chitkara.edu.in>
2025-07-09 11:30:23 +0530

    Collections done

--- 2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -22,11 +22,15 @@ if let janDays = days["Jan"] {
 }
 
 //:  Given the following arrays, create a new [String : [String]] dictionary. `shapesArray` should use the key "Shapes" and `colorsArray` should use the key "Colors." Print the resulting dictionary.
-
+var shapesArray: [String] = ["Square", "Circle", "Triangle"]
+var colorsArray: [String] = ["Red", "Blue", "Yellow"]
+var shapesAndColors: [String: [String]] = ["Shapes": shapesArray, "Colors": colorsArray]
+print(shapesAndColors)
 
 //:  Print the last element of `colorsArray`, accessing it through the dictionary you've created. You'll have to use if-let syntax or the force unwrap operator to unwrap what is returned from the dictionary before you can access an element of the array.
-
-
+if let lastColor=shapesAndColors["Colors"]?.last{
+    print(lastColor)
+}
 /*:
 [Previous](@previous)  |  page 3 of 4  |  [Next: App Exercise - Pacing](@next)
  */
