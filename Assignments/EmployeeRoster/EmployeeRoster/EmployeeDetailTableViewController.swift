14eeffb58dae06f045b5f3a851d2149b24e82dd4
MahiAwasthi <156103215+MahiAwasthi@users.noreply.github.com>
2025-09-16 09:36:54 +0530

    .

---
+++ Assignments/EmployeeRoster/EmployeeRoster/EmployeeDetailTableViewController.swift
@@ -0,0 +1,79 @@
+
+import UIKit
+
+protocol EmployeeDetailTableViewControllerDelegate: AnyObject {
+    func employeeDetailTableViewController(_ controller: EmployeeDetailTableViewController, didSave employee: Employee)
+}
+
+class EmployeeDetailTableViewController: UITableViewController, UITextFieldDelegate {
+
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var dobLabel: UILabel!
+    @IBOutlet var employeeTypeLabel: UILabel!
+    @IBOutlet var saveBarButtonItem: UIBarButtonItem!
+
+    @IBOutlet weak var dobDatePicker: UIDatePicker!
+   
+    var isEditingBirthday: Bool = false {
+        didSet {
+            if isEditingBirthday {
+                tableView.beginUpdates()
+                tableView.endUpdates()
+            }
+        }
+    }
+    
+    weak var delegate: EmployeeDetailTableViewControllerDelegate?
+    var employee: Employee?
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 2 && isEditingBirthday {
+            return 0
+        }
+        return UITableView.automaticDimension
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateView()
+        updateSaveButtonState()
+    }
+        
+    func updateView() {
+        if let employee = employee {
+            navigationItem.title = employee.name
+            nameTextField.text = employee.name
+            
+            dobLabel.text = employee.dateOfBirth.formatted(date: .abbreviated, time: .omitted)
+            dobLabel.textColor = .label
+            employeeTypeLabel.text = employee.employeeType.description
+            employeeTypeLabel.textColor = .label
+        } else {
+            navigationItem.title = "New Employee"
+        }
+    }
+    
+    private func updateSaveButtonState() {
+        let shouldEnableSaveButton = nameTextField.text?.isEmpty == false
+        saveBarButtonItem.isEnabled = shouldEnableSaveButton
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let name = nameTextField.text else {
+            return
+        }
+        
+        let employee = Employee(name: name, dateOfBirth: Date(), employeeType: .exempt)
+        delegate?.employeeDetailTableViewController(self, didSave: employee)
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        employee = nil
+    }
+
+    @IBAction func nameTextFieldDidChange(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+
+}
